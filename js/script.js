8505b513935189fd77e398aea44b3270240f5a7f
hfcosta <hfcosta>
2026-02-09 11:19:34 -0300

    alterações da última aula prática - manipulação de formulário

--- js/script.js
+++ js/script.js
@@ -1,5 +1,54 @@
-alert("Hello World!");
-console.log("primeiro print no console");
+const form = document.querySelector("#formMensagem");
+const input = document.querySelector("#mensagem");
+const erro = document.querySelector("#erro");
+const lista = document.querySelector("#lista");
+
+//"Banco de dados" em memória (array)
+let mensagens = [];
+
+function validarTexto(texto) {
+  const txt = texto.trim();
+
+  if (txt === "") {
+    return "Digite algo antes de enviar";
+  }
+
+  if (txt.length < 3) {
+    return "Mínimo de 3 caracteres";
+  }
+
+  return "";
+}
+
+//Renderizando/mostrando a lista na tela
+function render() {
+  lista.innerHTML = "";
+
+  //<li> para cada mensagem
+  for (const msg of mensagens) {
+    const li = document.createElement("li");
+    li.textContent = msg;
+    lista.append(li);
+  }
+}
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const textoDigitado = input.value;
+  const mensagemErro = validarTexto(textoDigitado);
+
+  if (mensagemErro !== "") {
+    erro.textContent = mensagemErro;
+    return;
+  }
+
+  mensagens.push(textoDigitado.trim());
+  render();
+
+  input.value = "";
+  input.focus();
+});
 
 //exemplo de função
 function falar() {
